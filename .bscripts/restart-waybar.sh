a298d985c45fbae5be60d8e226f836e9abc9f07a
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-07-10 11:15:55 +0700

    feat(waybar): add scheme colors and style waybar around

--- .bscripts/restart-waybar.sh
+++ .bscripts/restart-waybar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-pkill -SIGUSR2 waybar
-sleep 0.2
+
+self_pid=$$
+
+for pid in $(pgrep -x waybar); do
+  if [ "$pid" != "$self_pid" ]; then
+    kill "$pid"
+  fi
+done
+
+sleep 0.5
+
 waybar &
 
