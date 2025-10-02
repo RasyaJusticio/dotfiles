35cf5f65d18fda7c26c9eb54b1e3a6b155839bec
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-10-02 14:17:08 +0700

    feat(nvim/telescope): add fzf native plugin for faster searching

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,36 +1,44 @@
 local utils = require("core.utils")
 
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").load_extension("fzf")
 
-		utils.setKeymap("n", "<leader>lf", function()
-			builtin.find_files({
-				hidden = false,
-				no_ignore = false,
-			})
-		end, { desc = "Telescope: Find files" })
+			local builtin = require("telescope.builtin")
 
-		utils.setKeymap("n", "<leader>laf", function()
-			builtin.find_files({
-				hidden = true,
-				no_ignore = true,
-			})
-		end, { desc = "Telescope: Find all files" })
+			utils.setKeymap("n", "<leader>lf", function()
+				builtin.find_files({
+					hidden = false,
+					no_ignore = false,
+				})
+			end, { desc = "Telescope: Find files" })
 
-		utils.setKeymap("n", "<leader>le", function()
-			builtin.live_grep({})
-		end, { desc = "Telescope: Find text in files" })
+			utils.setKeymap("n", "<leader>laf", function()
+				builtin.find_files({
+					hidden = true,
+					no_ignore = true,
+				})
+			end, { desc = "Telescope: Find all files" })
 
-		utils.setKeymap("n", "<leader>lae", function()
-			builtin.live_grep({
-				additional_args = function(_)
-					return { "--no-ignore", "--hidden", "--follow" }
-				end,
-			})
-		end, { desc = "Telescope: Find text in all files" })
-	end,
+			utils.setKeymap("n", "<leader>le", function()
+				builtin.live_grep({})
+			end, { desc = "Telescope: Find text in files" })
+
+			utils.setKeymap("n", "<leader>lae", function()
+				builtin.live_grep({
+					additional_args = function(_)
+						return { "--no-ignore", "--hidden", "--follow" }
+					end,
+				})
+			end, { desc = "Telescope: Find text in all files" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
 }
