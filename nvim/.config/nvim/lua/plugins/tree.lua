f4b318f11b26778128f5f98f3900553c4930b076
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-10-06 09:48:02 +0700

    feat(nvim): close nvim-tree on file open

--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -4,7 +4,13 @@ return {
 	config = function()
 		local api = require("nvim-tree.api")
 
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		})
 
 		local function opts(desc)
 			return { desc = "nvim-tree: " .. desc, noremap = true, silent = true, nowait = true }
