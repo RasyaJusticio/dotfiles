689e44960ef02cfb593831bfb25b65da0695d48e
RasyaJusticio <rasyajusticio.public@gmail.com>
2025-10-13 10:05:42 +0700

    feat(nvim): add many more treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,42 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "vim",
-                "vimdoc",
-                "markdown",
-                "markdown_inline",
-                "html",
-                "css",
-                "javascript",
-                "typescript",
-                "php",
-                "blade"
-            },
-            sync_install = true,
-            auto_install = true,
-            highlight = {
-                enable = true
-            }
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"blade",
+				"css",
+				"diff",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"printf",
+				"php",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			sync_install = true,
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
 }
